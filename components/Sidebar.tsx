d71d0f9f768c8f7f9535ec37c74b02984d757fb7
idremouche anis <anisidrenmouche@hotmail.fr>
2026-05-12 22:11:23 +0200

    fix: duplicate import Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,6 @@
 'use client'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { LayoutDashboard, Briefcase, Star, ArrowUpDown, Sparkles, LogOut } from 'lucide-react'
 import { useClerk, useUser } from '@clerk/nextjs'
 import { LayoutDashboard, Briefcase, Star, ArrowUpDown, Sparkles, LogOut, User } from 'lucide-react'
 
